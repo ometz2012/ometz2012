8ab799676aa318e1a98f1f4d6c7007fedf3cd018
ometz2012 <goldman_alex@walla.com>
2012-08-29 13:41:28 -0700

    Eliya Added Example of GetCompanyByID()

--- Ometz.RFQ/Ometz.RFQ.BLL/Class1.cs
+++ Ometz.RFQ/Ometz.RFQ.BLL/Class1.cs
@@ -3,11 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Ometz.RFQ.DAL;
+using Ometz.RFQ.BLL.Model;
 
 namespace Ometz.RFQ.BLL
 {
     public class Class1
     {
+
+
         
+    }
+
+    public class DTOCompanyToShow : DTOCompany
+    {
+        public override string GetCompanyCategory()
+        {
+            int categoryID = this.CategoryID;
+            Category category;
+            using (var context = new RFQEntities())
+            {
+                category = (from cat in context.Categories
+                            where cat.CategoryID == categoryID
+                            select cat).First();
+
+            }
+
+            string categoryName = category.Type.ToString();
+            return categoryName;
+
+
+        }
+
+        public override string GetCompanyType()
+        {
+            int typeID = this.CompanyTypeID;
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BLLServices
+    {
+
+        //Get Company By ID Method
+        public static DTOCompanyToShow GetCompanyByID(int CompanyID)
+        {
+            Company CompanySpecific = new Company();
+            string path = "Category";
+            string path1 = "CompanyType";
+
+            using (var context = new RFQEntities())
+            {
+                CompanySpecific = (from comp in context.Companies.Include(path).Include(path1)
+                                   where comp.CompanyID == CompanyID
+                                   select comp).First();
+
+
+            }
+
+            DTOCompanyToShow CompanyToReturn = new DTOCompanyToShow();
+            CompanyToReturn.CompanyID = CompanySpecific.CompanyID;
+            CompanyToReturn.Name = CompanySpecific.Name;
+            CompanyToReturn.CategoryID = CompanySpecific.Category.CategoryID;
+            CompanyToReturn.CompanyTypeID = CompanySpecific.CompanyType.CompanyTypeID;
+
+
+
+            return CompanyToReturn;
+        }
     }
 }
